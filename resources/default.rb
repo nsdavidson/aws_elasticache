4e12997b9e5a5b9248d783823dd7b7025f3ee79d
Jeremy Koerber <jeremy@creativemarket.com>
2015-06-08 14:52:07 -0700

    remove required flag for aws creds - let aws-sdk use iam

--- resources/default.rb
+++ resources/default.rb
@@ -1,8 +1,8 @@
 actions :create, :update, :delete
 default_action :create
 
-attribute :aws_access_key, kind_of: String, required: true
-attribute :aws_secret_access_key, kind_of: String, required: true
+attribute :aws_access_key, kind_of: String
+attribute :aws_secret_access_key, kind_of: String
 attribute :region, kind_of: String, required: true
 attribute :cache_cluster_id, kind_of: String, name_attribute: true
 attribute :num_cache_nodes, kind_of: Integer, required: true
